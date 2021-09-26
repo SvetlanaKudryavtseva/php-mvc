91e15ea530d15a8f2046a39464204107bfb4471e
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-09-26 19:47:15 +0300

    Views created. Connection to database.

--- Controllers/About.php
+++ Controllers/About.php
@@ -1,5 +1,7 @@
 <?php
     class About extends Controller {
-        
+        public static function doSomething(){
+            print_r(self::query("SELECT * FROM cars"));
+        }
     }
 ?>
--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -1,8 +1,10 @@
 <?php
-    class Controller {
+    class Controller extends Database {
+
         public static function CreateView($viewName){
             // echo "View created!!!";
             require_once("./Views/$viewName.php");
+            static::doSomething();
         }
     }
 ?>
